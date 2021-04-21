0d0bd43bce2f291e572af91b35851f3944cf4584
folarmi <fola4ril@gmail.com>
2021-04-22 00:57:21 +0100

    landing page

--- src/atoms/BigText.js
+++ src/atoms/BigText.js
@@ -4,7 +4,7 @@ const BigText = ({ text, buttonText }) => {
   return (
     <section className="mt-8 bg-gray border border-primary rounded-lg p-4 lg:mb-32 text-center lg:mt-28 m-auto w-11/12">
       <HeaderTwo
-        className=" lg:mt-12 lg:text-6xl font-verybold font-extrabold"
+        className=" lg:mt-12  lg:text-6xl font-verybold font-extrabold"
         text={text}
       />
       <button class="bg-primary text-white text-xs font-bold py-4 px-10 rounded-md my-10 text-center">
--- src/atoms/HeaderTwo.js
+++ src/atoms/HeaderTwo.js
@@ -1,7 +1,7 @@
 const HeaderTwo = ({ text, className }) => {
   return (
-    <div className={` text-4xl ${className}`}>
-      <p className="leading-tight">{text}</p>
+    <div>
+      <p className={`leading-tight text-4xl ${className}`}>{text}</p>
     </div>
   );
 };
