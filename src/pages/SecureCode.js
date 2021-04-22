0162a070157b3f959861648a1241ff56ef31b990
folarmi <fola4ril@gmail.com>
2021-04-22 02:28:44 +0100

    worked on securecode page

--- src/pages/SecureCode.js
+++ src/pages/SecureCode.js
@@ -21,14 +21,16 @@ const SecureCode = () => {
           />
         </section>
 
-        <section className="border border-primary rounded-lg mt-12 secure mb-12 lg:mb-0">
+        <section className="border border-primary rounded-lg mt-12 mb-12 lg:mb-0">
           <div className="p-6 lg:p-12">
-            <InputTwo value="1" />
-            <InputTwo value="2" />
-            <InputTwo value="2" />
-            <InputTwo value="7" />
-            <InputTwo value="9" />
-            <InputTwo value="3" />
+            <div className="flex justify-between">
+              <InputTwo value="1" />
+              <InputTwo value="2" />
+              <InputTwo value="2" />
+              <InputTwo value="7" />
+              <InputTwo value="9" />
+              <InputTwo value="3" />
+            </div>
             <Link to="/codeverified">
               <button class="bg-primary text-white mt-8 w-full font-bold py-4 px-8 rounded-md">
                 Verify Code
--- src/templates/InputTwo.js
+++ src/templates/InputTwo.js
@@ -2,10 +2,12 @@ const InputTwo = ({ value, className }) => {
   return (
     <input
       type="text"
-      className={`border border-lightAsh w-10 h-10 input-right lg:mr-2 rounded text-center outline-none text-lg font-medium text-bodyGrey lg:w-14 lg:h-16 ${className}`}
+      className={`w-12 h-12 lg:w-14 lg:h-16 lg:mr-2 border border-lightAsh rounded text-center outline-none text-lg font-medium text-bodyGrey ${className}`}
       value={value}
     />
   );
 };
 
 export default InputTwo;
+// w-10 h-10
+// lg:w-14 lg:h-16
